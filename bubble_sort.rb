7af497ab3ee6628806b8cd2797573c6a9db2352a
aristides1000 <aristides1000@gmail.com>
2021-04-01 11:43:34 -0400

    Culmination of the Bubble Sort project

---
+++ bubble_sort.rb
@@ -0,0 +1,42 @@
+array_to_sort = [4, 3, 78, 2, 0, 2]
+
+def bubble_sort(array)
+  i = 0
+  while i < array.length
+    j = 1
+    while j < array.length
+      if array[j - 1] > array[j]
+        variable_for_exchanges = array[j - 1]
+        array[j - 1] = array[j]
+        array[j] = variable_for_exchanges
+      end
+      j += 1
+    end
+    i += 1
+  end
+  array
+end
+
+p bubble_sort(array_to_sort)
+
+def bubble_sort_by(array)
+  i = 0
+  while i < array.length
+    j = 1
+    while j < array.length
+      element_to_sorted = array[j - 1].length <=> array[j].length
+      if element_to_sorted == 1
+        variable_for_exchanges = array[j - 1]
+        array[j - 1] = array[j]
+        array[j] = variable_for_exchanges
+      end
+      j += 1
+    end
+    i += 1
+  end
+  array
+end
+
+p bubble_sort_by(%w[hi hello hey]) do |left, right|
+  left.length - right.length
+end
